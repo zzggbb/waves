4d53f2b9b86104778fe812e0da9b0f03fe03bb34
Zane Bradley <zanegbradley@gmail.com>
2018-03-12 02:38:06 -0700

    properly scale frequency axis

--- waves.py
+++ waves.py
@@ -19,11 +19,11 @@ class Waves(object):
         self.outputs = Outputs()
         self.stream = Stream(channels=1,
                              sample_rate=40 * 10**3,
-                             sample_size=1024)
+                             sample_size=2048)
         # visual params
-        self.background_color = pygame.Color(255, 255, 255)
-        self.colorA = pygame.Color(255, 0, 0)
-        self.colorB = pygame.Color(0, 0, 255)
+        self.background_color = pygame.Color(50, 50, 50)
+        self.colorA = pygame.Color(255, 0, 255)
+        self.colorB = pygame.Color(0, 255, 255)
         self.smooth = 0.5
         self.gain = 0.5
         self.num_bars = self.outputs.get_divisor()
@@ -52,19 +52,10 @@ class Waves(object):
 
     def draw_time_bars(self, samples, surface):
         width, height = surface.get_size()
-        bar_width = self.width / self.num_bars
-        bin_width = (self.stream.sample_size - 1) / (self.num_bars - 1)
-
-        # bar power methods:
-        # - maximum power of the bin
-        # - average of the bin
-        # - interpolated value of the (discrete) sampling function
-        #       - kind can be nearest, linear, or spline
-        power_function = lambda i: samples[round(i)]
+        bar_width = width / self.num_bars
 
         for i in range(self.num_bars):
-            x = i * bin_width
-            power_i = util.normalize(power_function(x))
+            power_i = util.normalize(samples[i])
             # smooth between current and last power
             power_s = self.t_history[i]*self.smooth + power_i*(1-self.smooth)
             # finally, smoothed and gain adjusted
@@ -81,19 +72,23 @@ class Waves(object):
     def draw_freq_bars(self, samples, surface):
         width, height = surface.get_size()
         length = self.stream.sample_size // 2
-        bar_width = width / length
+        bar_width = width / self.num_bars
 
         normalized = list(map(util.normalize, samples))
 
-        yf = numpy.abs(numpy.fft.fft(normalized)[1:length])
-        yf *= (2.0 / numpy.sqrt(self.stream.sample_size))
+        yf = numpy.log(numpy.abs(numpy.fft.fft(normalized))+1)/numpy.log(length)
 
-        for i in range(length - 1):
-            bar_height = yf[i] * height
+        x_max = self.num_bars - 1
+        for i in range(self.num_bars):
+            y = -1 * numpy.sqrt(
+                    (1 - (i**2)/(x_max**2)) * length**2
+                ) + length + 1
+            bar_height = yf[int(y)] * height
             top = height - bar_height
             left = i * bar_width
             rect = (left, top, bar_width, bar_height)
-            pygame.draw.rect(surface, self.colorA, rect)
+            color = util.gradient(i/self.num_bars, self.colorA, self.colorB)
+            pygame.draw.rect(surface, color, rect)
 
     def resize_bars(self):
         self.num_bars = self.outputs.get_divisor()
@@ -115,7 +110,10 @@ class Waves(object):
         GAIN_MIN = 0
         SMOOTH_DELTA = 0.01
         SMOOTH_MAX = 1
-        SMOOTH_MIN = 0
+        SMOOTH_MIN = 0 + GAIN_DELTA
+        RATE_DELTA = 2500
+        RATE_MIN = 0
+        SIZE_MIN = 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -138,16 +136,14 @@ class Waves(object):
                         self.gain += (k * GAIN_DELTA)
 
                 if event.key == ord('n'):
-                    if shift:
-                        self.stream.sample_size <<= 1
-                    else:
-                        self.stream.sample_size >>= 1
+                    k = 2 if shift else 0.5
+                    if self.stream.sample_size > SIZE_MIN:
+                        self.stream.sample_size *= k
 
                 if event.key == ord('r'):
-                    if shift:
-                        self.stream.sample_rate += 2500
-                    else:
-                        self.stream.sample_rate -= 2500
+                    k = 1 if shift else -1
+                    if self.stream.sample_rate > RATE_MIN:
+                        self.stream.sample_rate += k * 2500
 
                 if event.key == ord('s'):
                     if shift and self.smooth < SMOOTH_MAX:
@@ -163,7 +159,6 @@ class Waves(object):
                     self.resize_width()
 
     def exit(self):
-        print("quitting...")
         self.stream.close()
         pygame.display.quit()
         pygame.quit()
